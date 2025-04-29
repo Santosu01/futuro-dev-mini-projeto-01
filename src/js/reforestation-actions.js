7a0554718abc9e945b60e2fcdc54528e9cc2461d
Marcio Santos <santosu01@gmail.com>
2025-04-28 22:36:27 -0300

    feat: add reforest data page

---
+++ src/js/reforestation-actions.js
@@ -0,0 +1,81 @@
+const button = document.querySelector("button");
+
+const getData = () => {
+  const treeQuantityInputElement = document.getElementById("tree-quantity");
+  const treeTypeInputElement = document.getElementById("tree-type");
+
+  return {
+    treeQuantity: treeQuantityInputElement?.value,
+    treeType:
+      treeTypeInputElement?.value !== "null"
+        ? treeTypeInputElement?.value
+        : undefined,
+  };
+};
+
+const clearAllErrors = () => {
+  const fields = ["tree-quantity-input", "tree-type-input"];
+
+  fields.forEach((field) => {
+    const elementContainer = document.querySelector(
+      `#${field} .input-error-container`
+    );
+
+    if (elementContainer) {
+      elementContainer.textContent = "";
+    }
+  });
+};
+
+const setErrorText = (inputContainerId, message) => {
+  const elementContainer = document.querySelector(
+    `#${inputContainerId} .input-error-container`
+  );
+
+  if (elementContainer) {
+    const newElement = document.createElement("p");
+    newElement.textContent = message;
+    elementContainer.appendChild(newElement);
+  }
+};
+
+const setError = (input, message) => {
+  if (input === "treeQuantity") {
+    setErrorText("tree-quantity-input", message);
+  }
+
+  if (input === "treeType") {
+    setErrorText("tree-type-input", message);
+  }
+};
+
+const validateData = (data) => {
+  clearAllErrors();
+  let errors = 0;
+
+  if (!data.treeQuantity) {
+    setError("treeQuantity", "Arvores plantadas é obrigatório.");
+    errors += 1;
+  } else if (
+    Number.isNaN(Number(data.treeQuantity)) ||
+    !Number.isInteger(Number(data.treeQuantity))
+  ) {
+    setError("treeQuantity", "Informe um número inteiro válido.");
+    errors += 1;
+  }
+
+  if (!data.treeType) {
+    setError("treeType", "Tipo de arvore é obrigatório.");
+    errors += 1;
+  }
+
+  if (errors === 0) {
+    console.log("tudo ok!!");
+  }
+};
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  validateData(getData());
+});
